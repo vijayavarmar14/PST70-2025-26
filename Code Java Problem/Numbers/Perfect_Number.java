419a564938c850a1b80efa5f23d65c2659ff32dc
Vijaya Varma R <vijayavarmar14@gmail.com>
2026-02-11 12:23:57 +0530

    Add files via upload

---
+++ Code Java Problem/Numbers/Perfect_Number.java
@@ -0,0 +1,23 @@
+package numbers;
+import java.util.Scanner;
+public class Perfect_Number {
+
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        int num = sc.nextInt();
+	        int sum = 0;
+	        for (int i = 1; i <= num / 2; i++) {
+	            if (num % i == 0) {
+	                sum = sum + i;
+	            }
+	        }
+	        if (sum == num && num != 0) {
+	            System.out.println(num + " is a Perfect Number.");
+	        } else {
+	            System.out.println(num + " is NOT a Perfect Number.");
+	        }
+
+	        sc.close();
+	    }
+	}
