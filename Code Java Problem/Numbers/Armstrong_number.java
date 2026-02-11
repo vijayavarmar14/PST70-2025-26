cf571664fe13e6041fc80e49c3013fee98eabac9
Vijaya Varma R <vijayavarmar14@gmail.com>
2026-02-11 12:22:28 +0530

    Add files via upload

---
+++ Code Java Problem/Numbers/Armstrong_number.java
@@ -0,0 +1,28 @@
+package numbers;
+import java.util.Scanner;
+public class Armstrong_number {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int a=in.nextInt();
+		int x=a;
+		int sum=0,count=0;
+		int temp=a;
+	     {
+	      while (temp > 0) {
+	       temp = temp / 10; 
+	       count++;
+	       }
+	    }
+	     while (a>0) {
+	    	 sum+=(int)Math.pow(a%10,count);
+	    	 a/=10;
+	     }
+	     if (x==sum)
+	     System.out.println("Armstrong");
+	     else
+	    	 System.out.println("Not armstrong");
+		in.close();
+	}
+}
